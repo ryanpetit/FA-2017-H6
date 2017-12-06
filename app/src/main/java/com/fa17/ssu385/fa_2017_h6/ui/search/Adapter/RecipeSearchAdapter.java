66a5ee01aee51b1fb1642f98da8c28d0d949bc58
Hayley Gerard <hayleygerard@wrls-102-219.sonoma.edu>
2017-12-05 19:27:44 -0800

    not working completely

---
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h6/ui/search/Adapter/RecipeSearchAdapter.java
@@ -0,0 +1,8 @@
+package com.fa17.ssu385.fa_2017_h6.ui.search.Adapter;
+
+/**
+ * Created by hayleygerard on 12/5/17.
+ */
+
+public class RecipeSearchAdapter {
+}
---
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h6/ui/search/RecipeDetails/RecipeDetailActivity.java
@@ -0,0 +1,8 @@
+package com.fa17.ssu385.fa_2017_h6.ui.search.RecipeDetails;
+
+/**
+ * Created by hayleygerard on 12/5/17.
+ */
+
+public class RecipeDetailActivity {
+}
