522b1e551753d8104bf1d16db3f38004f4634741
Blake Vollmer <blakeavollmer@gmail.com>
2017-12-05 21:06:07 -0800

    help plz

--- app/src/main/java/com/fa17/ssu385/fa_2017_h6/ui/search/Interactor/RecipeSearchInteractorImpl.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h6/ui/search/Interactor/RecipeSearchInteractorImpl.java
@@ -1,21 +1,14 @@
 package com.fa17.ssu385.fa_2017_h6.ui.search.Interactor;
 
+import com.fa17.ssu385.fa_2017_h6.model.Recipe;
+
 /**
  * Created by blakevollmer on 11/30/17.
  */
 
-public class RecipeSearchInteractorImpl {
-    RecipeSearchInteractor recipeSearchInteractor = new RecipeSearchInteractor() {
-        @Override
-        public void getRecipe(String string, OnSearchResponse response) {
-
-        }
-    };
-
-    RecipeSearchInteractor responseListener;
-    public void setResponseListener(RecipeSearchInteractor responseListener) {
-        this.responseListener = responseListener;
+public class RecipeSearchInteractorImpl implements RecipeSearchInteractor {
+    @Override
+    public void getRecipe(String string, OnSearchResponse response) {
+        response.callback(new Recipe());
     }
-
-
 }
--- app/src/main/java/com/fa17/ssu385/fa_2017_h6/ui/search/SearchActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h6/ui/search/SearchActivity.java
@@ -13,13 +13,18 @@ import com.fa17.ssu385.fa_2017_h6.R;
 import com.fa17.ssu385.fa_2017_h6.model.Recipe;
 import com.fa17.ssu385.fa_2017_h6.model.RecipeList;
 import com.fa17.ssu385.fa_2017_h6.network.RecipeSearchAsyncTask;
+import com.fa17.ssu385.fa_2017_h6.ui.search.Interactor.RecipeSearchInteractor;
+import com.fa17.ssu385.fa_2017_h6.ui.search.Presenter.SearchPresenter;
 import com.fa17.ssu385.fa_2017_h6.ui.search.view.SearchView;
+import com.fa17.ssu385.fa_2017_h6.ui.search.Interactor.RecipeSearchInteractorImpl;
+
 
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
+
 
     // Butterknife used to bind view elements
     @BindView(R.id.my_search_button)
